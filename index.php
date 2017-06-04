1dfd43c6da906385143d5489f976ecf23149e9dc
Marko Esna <mesna@itcollege.ee>
2017-06-04 11:20:57 +0300

    progress

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
+<meta charset="utf-8">
 <html>
 <head>
 	<title>Salajane parool</title>
 </head>
 <body>
-
+	<span>Kui soovid näha faili sisu, sisesta parool!!</span>
+	<form action="kontrolli_parooli.php" method="post">
+		<input type="password" name="parool">
+		<input type="submit" name="kontrolli" value="Ava">
+	</form>
 </body>
-</html>>
+</html>
